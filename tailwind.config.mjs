7e62490ed2d215652a172c9024f3d2362e0e6c0b
Sebinmon vr <sebinmonvr@gmail.com>
2024-12-08 21:59:16 +0530

    desktop

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,22 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+        primary: '#92193b', // Add your primary color
+      },
+      fontFamily: {
+        poppins: ['Poppins'], // Add Poppins as the default font
+      },
+			backgroundImage: {
+        'one': "url('/images/1.svg')",
+        'two': "url('/images/2.svg')",
+        'three': "url('/images/3.svg')",
+      },
+			
+		},
 	},
 	plugins: [],
 }
+
+
