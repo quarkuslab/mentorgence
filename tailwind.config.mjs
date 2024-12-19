bda431670321222792cd7f87232bda15915bc291
Sebinmon vr <sebinmonvr@gmail.com>
2024-12-19 16:52:13 +0530

    Co-authored-by: Mohamed Ihssan <ihsanvp2738@gmail.com>

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,16 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
 			colors: {
-        primary: '#92193b', // Add your primary color
+        primary: '#92193b',
+        customblue: '#0F2F64',
       },
       fontFamily: {
-        poppins: ['Poppins'], // Add Poppins as the default font
+        poppins: ['Poppins', ...defaultTheme.fontFamily.sans], // Add Poppins as the default font
       },
 			backgroundImage: {
         'one': "url('/images/1.svg')",
